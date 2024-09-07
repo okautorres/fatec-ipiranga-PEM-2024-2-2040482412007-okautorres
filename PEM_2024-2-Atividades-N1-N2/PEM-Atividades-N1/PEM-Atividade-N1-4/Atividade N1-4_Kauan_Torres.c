6f265074cbfbac7ab14485ba88e3ed9734e878d6
okautorres <darqueboost@gmail.com>
2024-09-06 21:01:44 -0300

    atividade n1-4

---
+++ PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N1/PEM-Atividade-N1-4/Atividade N1-4_Kauan_Torres.c
@@ -0,0 +1,264 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*-----------------------------------------------------------------------------------------*
+* Disciplina: Programaçao Estruturada e Modular                                            *
+*          Prof. Carlos Veríssimo                                                          *
+*------------------------------------------------------------------------------------------*
+* Objetivo do Programa: Programa em C para demonstrar o xeque pastor                       *
+* Data - 02/09/2024                                                                        * 
+* Autor: Kauan Torres                                                                      *
+*------------------------------------------------------------------------------------------*/
+
+int main() {
+	char tabuleiro[64][4]; //Vetor com a quantidade de peças/casas[quantidade de caracteres]
+	int x;
+	
+	for (x = 0; x < 64; x++) {
+        strcpy(tabuleiro[x], " "); //Adicionando um X em todas as casas, para posteriormente completa-lás
+    }
+    
+    // Peças brancas
+    strcpy(tabuleiro[0], "BT1"); strcpy(tabuleiro[1], "BC1"); strcpy(tabuleiro[2], "BB1");  strcpy(tabuleiro[3], "BD1"); strcpy(tabuleiro[4], "BR1"); strcpy(tabuleiro[5], "BB2"); strcpy(tabuleiro[6], "BC2"); strcpy(tabuleiro[7], "BT2");
+	strcpy(tabuleiro[8], "BP1"); strcpy(tabuleiro[9], "BP2"); strcpy(tabuleiro[10], "BP3"); strcpy(tabuleiro[11], "BP4"); strcpy(tabuleiro[12], "BP5"); strcpy(tabuleiro[13], "BP6"); strcpy(tabuleiro[14], "BP7"); strcpy(tabuleiro[15], "BP8");
+	
+	// Peças pretas
+	strcpy(tabuleiro[48], "PP1"); strcpy(tabuleiro[49], "PP2"); strcpy(tabuleiro[50], "PP3"); strcpy(tabuleiro[51], "PP4"); strcpy(tabuleiro[52], "PP5"); strcpy(tabuleiro[53], "PP6"); strcpy(tabuleiro[54], "PP7"); strcpy(tabuleiro[55], "PP8");
+	strcpy(tabuleiro[56], "PT1"); strcpy(tabuleiro[57], "PC1");  strcpy(tabuleiro[58], "PB1");  strcpy(tabuleiro[59], "PD1"); strcpy(tabuleiro[60], "PR1"); strcpy(tabuleiro[61], "PB2"); strcpy(tabuleiro[62], "PC2"); strcpy(tabuleiro[63], "PT2");
+
+	//Colunas do tabuleiro
+     char colunas[] = "abcdefgh";
+     int Ncolunas = 8;
+        printf("========================================== \n \n");
+        printf("      POSICAO INICIAL DO TABULEIRO \n \n");
+        printf("========================================== \n");
+    
+    // Letras das colunas do tabuleiro
+    printf("  ");
+    for (x = 0; x < Ncolunas; x++) {
+        printf("  %c  ", colunas[x]);
+    }
+    printf("\n");
+    
+    //Linhas do tabuleiro
+    for (x = 7; x >= 0; x--) {
+        printf("%d ", x + 1); // Impressão do número da linha
+        
+        //Impressão das peças
+        int y;
+        for (Ncolunas = 0; Ncolunas < 8; Ncolunas++) {
+            int i = x * 8 + Ncolunas; //Index das peças. Sendo x o número da linha*8+número da coluna.
+            printf("%4s ", tabuleiro[i]);
+        }
+        printf("\n");
+    }
+    
+    printf("========================================== \n \n");
+    printf("      JOGADA #1 \n \n");
+    printf("========================================== \n \n");
+    
+    printf("------Brancas jogam e4 \n \n");
+    
+    
+    
+    // Letras das colunas do tabuleiro
+    printf("  ");
+    for (x = 0; x < Ncolunas; x++) {
+        printf("  %c  ", colunas[x]);
+    }
+    printf("\n");
+    
+    strcpy(tabuleiro[12], "...");
+    strcpy(tabuleiro[28], "BP5");
+    
+    //Linhas do tabuleiro
+    for (x = 7; x >= 0; x--) {
+        printf("%d ", x + 1); // Impressão do número da linha
+        
+        //Impressão das peças
+        int y;
+        for (Ncolunas = 0; Ncolunas < 8; Ncolunas++) {
+            int i = x * 8 + Ncolunas; //Index das peças. Sendo x o número da linha*8+número da coluna.
+            printf("%4s ", tabuleiro[i]);
+        }
+        printf("\n");
+    }
+    
+    printf("\n ------Pretas jogam e5 \n \n");
+    
+    
+    
+    // Letras das colunas do tabuleiro
+    printf("  ");
+    for (x = 0; x < Ncolunas; x++) {
+        printf("  %c  ", colunas[x]);
+    }
+    printf("\n");
+    
+    strcpy(tabuleiro[52], "...");
+    strcpy(tabuleiro[36], "PP5");
+    
+    //Linhas do tabuleiro
+    for (x = 7; x >= 0; x--) {
+        printf("%d ", x + 1); // Impressão do número da linha
+        
+        //Impressão das peças
+        int y;
+        for (Ncolunas = 0; Ncolunas < 8; Ncolunas++) {
+            int i = x * 8 + Ncolunas; //Index das peças. Sendo x o número da linha*8+número da coluna.
+            printf("%4s ", tabuleiro[i]);
+        }
+        printf("\n");
+    }
+    
+    printf("========================================== \n \n");
+    printf("      JOGADA #2 \n \n");
+    printf("========================================== \n \n");
+    
+    printf("------Brancas jogam Bc4 \n \n");
+    
+    
+    
+    // Letras das colunas do tabuleiro
+    printf("  ");
+    for (x = 0; x < Ncolunas; x++) {
+        printf("  %c  ", colunas[x]);
+    }
+    printf("\n");
+    
+    strcpy(tabuleiro[5], "...");
+    strcpy(tabuleiro[26], "BB2");
+    
+    //Linhas do tabuleiro
+    for (x = 7; x >= 0; x--) {
+        printf("%d ", x + 1); // Impressão do número da linha
+        
+        //Impressão das peças
+        int y;
+        for (Ncolunas = 0; Ncolunas < 8; Ncolunas++) {
+            int i = x * 8 + Ncolunas; //Index das peças. Sendo x o número da linha*8+número da coluna.
+            printf("%4s ", tabuleiro[i]);
+        }
+        printf("\n");
+    }
+    
+    printf("\n ------Pretas jogam Cc6 \n \n");
+    
+    
+    
+    // Letras das colunas do tabuleiro
+    printf("  ");
+    for (x = 0; x < Ncolunas; x++) {
+        printf("  %c  ", colunas[x]);
+    }
+    printf("\n");
+    
+    strcpy(tabuleiro[57], "...");
+    strcpy(tabuleiro[42], "PC1");
+    
+    //Linhas do tabuleiro
+    for (x = 7; x >= 0; x--) {
+        printf("%d ", x + 1); // Impressão do número da linha
+        
+        //Impressão das peças
+        int y;
+        for (Ncolunas = 0; Ncolunas < 8; Ncolunas++) {
+            int i = x * 8 + Ncolunas; //Index das peças. Sendo x o número da linha*8+número da coluna.
+            printf("%4s ", tabuleiro[i]);
+        }
+        printf("\n");
+    }
+    
+    printf("========================================== \n \n");
+    printf("      JOGADA #3 \n \n");
+    printf("========================================== \n \n");
+    
+    printf("------Brancas jogam Dh5 \n \n");
+    
+    
+    
+    // Letras das colunas do tabuleiro
+    printf("  ");
+    for (x = 0; x < Ncolunas; x++) {
+        printf("  %c  ", colunas[x]);
+    }
+    printf("\n");
+    
+    strcpy(tabuleiro[3], "...");
+    strcpy(tabuleiro[39], "BD1");
+    
+    //Linhas do tabuleiro
+    for (x = 7; x >= 0; x--) {
+        printf("%d ", x + 1); // Impressão do número da linha
+        
+        //Impressão das peças
+        int y;
+        for (Ncolunas = 0; Ncolunas < 8; Ncolunas++) {
+            int i = x * 8 + Ncolunas; //Index das peças. Sendo x o número da linha*8+número da coluna.
+            printf("%4s ", tabuleiro[i]);
+        }
+        printf("\n");
+    }
+    
+    printf("\n ------Pretas jogam Cf6 \n \n");
+    
+    
+    
+    // Letras das colunas do tabuleiro
+    printf("  ");
+    for (x = 0; x < Ncolunas; x++) {
+        printf("  %c  ", colunas[x]);
+    }
+    printf("\n");
+    
+    strcpy(tabuleiro[62], "...");
+    strcpy(tabuleiro[45], "PC2");
+    
+    //Linhas do tabuleiro
+    for (x = 7; x >= 0; x--) {
+        printf("%d ", x + 1); // Impressão do número da linha
+        
+        //Impressão das peças
+        int y;
+        for (Ncolunas = 0; Ncolunas < 8; Ncolunas++) {
+            int i = x * 8 + Ncolunas; //Index das peças. Sendo x o número da linha*8+número da coluna.
+            printf("%4s ", tabuleiro[i]);
+        }
+        printf("\n");
+    }
+    
+    printf("========================================== \n \n");
+    printf("      JOGADA #4 (XEQUE PASTOR) \n \n");
+    printf("========================================== \n \n");
+    
+    printf("------Brancas capturam peao PP6 em f7 e XEQUE MATE.  \n \n");
+    
+    
+    
+    // Letras das colunas do tabuleiro
+    printf("  ");
+    for (x = 0; x < Ncolunas; x++) {
+        printf("  %c  ", colunas[x]);
+    }
+    printf("\n");
+    
+    strcpy(tabuleiro[39], "...");
+    strcpy(tabuleiro[53], "BD1");
+    
+    //Linhas do tabuleiro
+    for (x = 7; x >= 0; x--) {
+        printf("%d ", x + 1); // Impressão do número da linha
+        
+        //Impressão das peças
+        int y;
+        for (Ncolunas = 0; Ncolunas < 8; Ncolunas++) {
+            int i = x * 8 + Ncolunas; //Index das peças. Sendo x o número da linha*8+número da coluna.
+            printf("%4s ", tabuleiro[i]);
+        }
+        printf("\n");
+    }
+    
+    
+
+}
